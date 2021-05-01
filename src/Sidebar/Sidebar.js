d2982de5d66261cc2ac1c287ecfd66939f082120
Abhay Patil <71277980+Abhay5855@users.noreply.github.com>
2021-05-01 19:19:09 +0530

    docs: cheking if notes are null or not and then adding return statement in if clause

--- src/Sidebar/Sidebar.js
+++ src/Sidebar/Sidebar.js
@@ -16,39 +16,43 @@ class SidebarComponent extends React.Component {
   render() {
     const { notes, classes, selectedNoteIndex } = this.props;
 
-    return (
-      <div className={classes.SidebarContainer}>
-        <Button onClick={this.newNoteBtnClick} className={classes.newNoteBtn}>
-          {this.state.addingNote ? "Cancel" : "New Note"}
-        </Button>
-        {this.state.addingNote ? (
-          <div>
-            <input
-              className={classes.newNoteInput}
-              type="text"
-              placeholder="Add a new Title"
-              onKeyUp={(e) => this.updateTitle(e.target.value)}
-            ></input>
-          </div>
-        ) : null}
-        <List>
-          {notes.map((_note, _index) => {
-            return (
-              <div key={_index}>
-                <SidebarItemComponent
-                  _note={_note}
-                  _index={_index}
-                  selectedNoteIndex={selectedNoteIndex}
-                  selectNote={this.selectNote}
-                  deleteNote={this.deleteNote}
-                ></SidebarItemComponent>
-                <Divider></Divider>
-              </div>
-            );
-          })}
-        </List>
-      </div>
-    );
+    if (notes) {
+      return (
+        <div className={classes.SidebarContainer}>
+          <Button onClick={this.newNoteBtnClick} className={classes.newNoteBtn}>
+            {this.state.addingNote ? "Cancel" : "New Note"}
+          </Button>
+          {this.state.addingNote ? (
+            <div>
+              <input
+                className={classes.newNoteInput}
+                type="text"
+                placeholder="Add a new Title"
+                onKeyUp={(e) => this.updateTitle(e.target.value)}
+              ></input>
+            </div>
+          ) : null}
+          <List>
+            {notes.map((_note, _index) => {
+              return (
+                <div key={_index}>
+                  <SidebarItemComponent
+                    _note={_note}
+                    _index={_index}
+                    selectedNoteIndex={selectedNoteIndex}
+                    selectNote={this.selectNote}
+                    deleteNote={this.deleteNote}
+                  ></SidebarItemComponent>
+                  <Divider></Divider>
+                </div>
+              );
+            })}
+          </List>
+        </div>
+      );
+    } else {
+      return <div></div>;
+    }
   }
 
   newNoteBtnClick = () => {
