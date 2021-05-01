e886b1aeadc5af386339b2c4fc0ebf427b92c0b4
Abhay Patil <71277980+Abhay5855@users.noreply.github.com>
2021-05-01 11:42:07 +0530

    feat: Import firebaseconfig

--- src/App.js
+++ src/App.js
@@ -1,4 +1,19 @@
 import "./styles.css";
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAELfdJtouh953hkDwlsiS6c11LgvWEDLo",
+  authDomain: "evernote-clone-ce765.firebaseapp.com",
+  projectId: "evernote-clone-ce765",
+  storageBucket: "evernote-clone-ce765.appspot.com",
+  messagingSenderId: "1021030373435",
+  appId: "1:1021030373435:web:4504c07b0f8978d7c5641e"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+
 
 export default function App() {
   return (
