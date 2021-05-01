a56d6d4ce7642c02460b4fed39e31e7a52c32c24
Abhay Patil <71277980+Abhay5855@users.noreply.github.com>
2021-05-01 11:46:46 +0530

    feat: Import firebaseconfig

--- src/App.js
+++ src/App.js
@@ -1,24 +1,6 @@
 import "./styles.css";
-import firebase from 'firebase/app';
-import 'firebase/firestore';
-
-const firebaseConfig = {
-  apiKey: "AIzaSyAELfdJtouh953hkDwlsiS6c11LgvWEDLo",
-  authDomain: "evernote-clone-ce765.firebaseapp.com",
-  projectId: "evernote-clone-ce765",
-  storageBucket: "evernote-clone-ce765.appspot.com",
-  messagingSenderId: "1021030373435",
-  appId: "1:1021030373435:web:4504c07b0f8978d7c5641e"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-
-
+import firebase from "firebase/app";
 
 export default function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+  return <div className="App"></div>;
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom";
-
+import firebase from "firebase/app";
+import "firebase/firestore";
 import App from "./App";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAELfdJtouh953hkDwlsiS6c11LgvWEDLo",
+  authDomain: "evernote-clone-ce765.firebaseapp.com",
+  projectId: "evernote-clone-ce765",
+  storageBucket: "evernote-clone-ce765.appspot.com",
+  messagingSenderId: "1021030373435",
+  appId: "1:1021030373435:web:4504c07b0f8978d7c5641e"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
